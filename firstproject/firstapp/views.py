8a09effe8e1d96d5a1b86a1ab32389efb4807b03
sadak <sadakkamal@gmail.com>
2023-07-28 01:54:26 +0530

    first commit by sadak

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def display(request):
+    print("Hi this is django")
+    s="<h1>Dont feel difficult....Just it is first time feeling... really django is very esay if u practice then you see</h1>"
+    return HttpResponse(s)
